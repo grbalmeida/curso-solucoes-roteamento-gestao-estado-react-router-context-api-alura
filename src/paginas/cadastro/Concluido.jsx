1d130565723b008911dd6711df27cb170224256f
grbalmeida <g.r.almeida@live.com>
2023-10-23 17:54:14 -0300

    Últimos detalhes visuais

--- src/paginas/cadastro/Concluido.jsx
+++ src/paginas/cadastro/Concluido.jsx
@@ -1,11 +1,40 @@
+import { Link as RouterLink } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { Row, Col } from 'react-grid-system';
+
 import { Tipografia } from 'componentes/Tipografia/Tipografia';
+import { Botao } from 'componentes/Botao/Botao';
+
+import conclusao from './assets/cliente-concluido.png';
+
+const ImagemEstilizada = styled.img`
+    max-width: 100%;
+    border-radius: 16px;
+`;
 
 const Concluido = () => {
     return (
         <>
-            <Tipografia variante="h1" componente="h1">
-                Seu perfil está completo
-            </Tipografia>
+            <div style={{ textAlign: 'center' }}>
+                <Tipografia variante="h1" componente="h1">
+                    Seu perfil está completo
+                </Tipografia>
+                <Tipografia variante="h3" componente="h3">
+                    Agora é só começar a se conectar com os melhores freelancers do mercado!
+                </Tipografia>
+            </div>
+            <figure>
+                <ImagemEstilizada src={conclusao} alt="" />
+            </figure>
+            <Row justify="center">
+                <Col lg={6} md={6} sm={6} style={{ textAlign: 'center' }}>
+                    <RouterLink to='/cadastro'>
+                        <Botao variante="secundaria">
+                            Voltar para a home
+                        </Botao>
+                    </RouterLink>
+                </Col>
+            </Row>
         </>
     )
 }
