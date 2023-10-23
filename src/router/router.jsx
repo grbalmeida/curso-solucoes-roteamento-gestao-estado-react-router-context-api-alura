353372a297360f037bc442f5313b6c9cacf5996c
grbalmeida <g.r.almeida@live.com>
2023-10-23 11:08:31 -0300

    Analisando e planejando

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from 'react-router-dom';
-import PaginaInicial from '../paginas/PaginaInicial';
 
 export const router = createBrowserRouter([
     {
-        path: "/",
-        element: <PaginaInicial />,
-        children: [],
+        path: '/',
+        children: [
+            {
+                path: 'cadastro',
+                element: <h1>Seleção de tipo de cliente</h1>,
+                children: [
+                    {
+                        path: 'cliente',
+                        element: <h1>Interesses</h1>
+                    },
+                    {
+                        path: 'dados-pessoais',
+                        element: <h1>Dados pessoais</h1>
+                    },
+                    {
+                        path: 'concluido',
+                        element: <h1>Concluído</h1>
+                    }
+                ]
+            }
+        ],
     }
 ]);
